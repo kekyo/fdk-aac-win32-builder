65fea371d3171c369cbfd432d8824e7b38da50e1
Kouji Matsui <k@kekyo.net>
2020-07-01 22:44:06 +0900

    FDK distributions will pull from this repo because makes stable building.

--- setup.sh
+++ setup.sh
@@ -8,11 +8,15 @@ mkdir stage
 
 cd artifacts
 
-wget https://jaist.dl.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-0.1.6.tar.gz
-wget https://jaist.dl.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.1.tar.gz
-wget https://github.com/nu774/fdkaac/archive/1.0.0.tar.gz
+# wget https://jaist.dl.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-0.1.6.tar.gz
+# wget https://jaist.dl.sourceforge.net/project/opencore-amr/fdk-aac/fdk-aac-2.0.1.tar.gz
+# wget https://github.com/nu774/fdkaac/archive/1.0.0.tar.gz
 
-mv 1.0.0.tar.gz fdkaac-1.0.0.tar.gz
+# mv 1.0.0.tar.gz fdkaac-1.0.0.tar.gz
+
+wget https://github.com/kekyo/fdk-aac-win32-builder/releases/download/distributes/fdk-aac-0.1.6.tar.gz
+wget https://github.com/kekyo/fdk-aac-win32-builder/releases/download/distributes/fdk-aac-2.0.1.tar.gz
+wget https://github.com/kekyo/fdk-aac-win32-builder/releases/download/distributes/fdkaac-1.0.0.tar.gz
 
 cd ..
 
